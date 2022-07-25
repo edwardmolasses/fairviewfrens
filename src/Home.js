025f528b3d8dea8d39d33a5c408ed9ce81b4b69b
Andrew Lachkovics <andrew.lachkovics@arcteryx.com>
2022-07-25 16:13:20 -0700

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles/globals.css'
+
+const Home = () => {
+    return (
+        <h3 className="bg-red-700">Home Page</h3>
+    );
+};
+
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './styles/globals.css'
+
+import Home from './Home';
+
+render(<Home />, document.getElementById('root'));
