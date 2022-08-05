6cb41c06e5643d0f71894ef5cbbfba548b9e94b3
Andrew Lachkovics <andrew.lachkovics@arcteryx.com>
2022-08-05 15:14:11 -0700

    add goose logo to 404 page

--- src/PageNotFound.js
+++ src/PageNotFound.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import logo from './images/puddlegoose-outlined-logo.png'
 import './styles/globals.css';
 
 export default function PageNotFound() {
@@ -7,6 +8,9 @@ export default function PageNotFound() {
         <div className="grid h-screen place-items-center">
             <div className="font-coiny uppercase font-bold text-white text-3xl">
                 Sorry no frens here, <Link className="text-rose-500 hover:text-white" to="/">maybe try over here!</Link>
+            </div>
+            <div className='content-center'>
+                <img className="mt-0 pb-10 sm:mt-20" src={logo} alt="" />
             </div>
         </div>
     )
