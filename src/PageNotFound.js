609672af933d6f75736182000c2319851b727f13
Andrew Lachkovics <andrew.lachkovics@arcteryx.com>
2022-08-05 16:51:37 -0700

    add title outline

--- src/PageNotFound.js
+++ src/PageNotFound.js
@@ -7,7 +7,7 @@ export default function PageNotFound() {
     return (
         <div className="grid h-screen place-items-center">
             <div className="font-coiny uppercase font-bold text-white text-3xl">
-                Sorry no frens here, <Link className="text-rose-500 hover:text-white" to="/">maybe try over here!</Link>
+                Sorry no frens here, <Link className="text-rose-500 hover:text-white transition ease-in-out delay-30" to="/">maybe try over here!</Link>
             </div>
             <div className='content-center'>
                 <img className="mt-0 pb-10 sm:mt-20" src={logo} alt="" />
