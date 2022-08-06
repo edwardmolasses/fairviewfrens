aae573bac4776bc1b0e8e94f2d8a2dfa0c925c56
Andrew Lachkovics <andrew.lachkovics@arcteryx.com>
2022-08-05 17:13:42 -0700

    add header component

--- src/Mint.js
+++ src/Mint.js
@@ -6,6 +6,8 @@ import { config } from '../dapp.config'
 import foff from './images/foff.gif'
 import './styles/globals.css';
 
+import Header from './Header';
+
 import {
   getTotalMinted,
   getMaxSupply,
@@ -126,161 +128,164 @@ export default function Mint() {
   }
 
   return (
-    <div className="min-h-screen h-full w-full overflow-hidden flex flex-col items-center justify-center">
-      <div className="relative w-full h-full flex flex-col items-center justify-center">
+    <div className="min-h-screen h-full w-full flex flex-col overflow-hidden">
+      <Header />
+      <div className="mt-20 w-full overflow-hidden flex flex-col items-center justify-center">
+        <div className="relative w-full h-full flex flex-col items-center justify-center">
 
-        <div className="flex flex-col items-center justify-center h-full w-full px-2 md:px-10">
-          <div className="relative z-1 md:max-w-3xl w-full bg-gray-900/90 filter backdrop-blur-sm py-4 rounded-md px-2 md:px-10 flex flex-col items-center">
-            {wallet && (
-              <button
-                className="absolute right-4 bg-white text-indigo-800 transition duration-200 ease-in-out font-chalk border-2 border-[rgba(0,0,0,1)] shadow-[0px_3px_0px_0px_rgba(0,0,0,1)] active:shadow-none px-4 py-2 rounded-md text-sm text-white tracking-wide uppercase"
-                onClick={() =>
-                  disconnect({
-                    label: wallet.label
-                  })
-                }
-              >
-                Disconnect
-              </button>
-            )}
-            <h1 className="font-coiny uppercase font-bold text-3xl md:text-4xl bg-gradient-to-br from-brand-green to-brand-blue bg-clip-text text-transparent mt-3">
-              {paused ? 'Paused' : isPreSale ? 'Pre-Sale' : 'Public Sale'}
-            </h1>
-            <h3 className="text-sm text-pink-200 tracking-widest">
-              {wallet?.accounts[0]?.address
-                ? wallet?.accounts[0]?.address.slice(0, 8) +
-                '...' +
-                wallet?.accounts[0]?.address.slice(-4)
-                : ''}
-            </h3>
+          <div className="flex flex-col items-center justify-center h-full w-full px-2 md:px-10">
+            <div className="relative z-1 md:max-w-3xl w-full bg-gray-900/90 filter backdrop-blur-sm py-4 rounded-md px-2 md:px-10 flex flex-col items-center">
+              {wallet && (
+                <button
+                  className="absolute right-4 bg-white text-black transition duration-200 ease-in-out font-chalk border-2 border-[rgba(0,0,0,1)] shadow-[0px_3px_0px_0px_rgba(0,0,0,1)] active:shadow-none px-4 py-2 rounded-md text-sm text-white tracking-wide uppercase"
+                  onClick={() =>
+                    disconnect({
+                      label: wallet.label
+                    })
+                  }
+                >
+                  Disconnect
+                </button>
+              )}
+              <h1 className="font-coiny uppercase font-bold text-3xl md:text-4xl bg-gradient-to-br from-brand-green to-brand-blue bg-clip-text text-transparent mt-3">
+                {paused ? 'Paused' : isPreSale ? 'Pre-Sale' : 'Public Sale'}
+              </h1>
+              <h3 className="text-sm text-pink-200 tracking-widest">
+                {wallet?.accounts[0]?.address
+                  ? wallet?.accounts[0]?.address.slice(0, 8) +
+                  '...' +
+                  wallet?.accounts[0]?.address.slice(-4)
+                  : ''}
+              </h3>
 
-            <div className="flex flex-col md:flex-row md:space-x-14 w-full mt-10 md:mt-14">
-              <div className="relative w-full">
-                <div className="font-coiny z-10 absolute top-2 left-2 opacity-80 filter backdrop-blur-lg text-base px-4 py-2 bg-black border border-white rounded-md flex items-center justify-center text-white font-semibold">
-                  <p>
-                    <span className="text-cyan-500">{totalMinted}</span> /{' '}
-                    {maxSupply}
-                  </p>
+              <div className="flex flex-col md:flex-row md:space-x-14 w-full mt-10 md:mt-14">
+                <div className="relative w-full">
+                  <div className="font-coiny z-10 absolute top-2 left-2 opacity-80 filter backdrop-blur-lg text-base px-4 py-2 bg-black border border-white rounded-md flex items-center justify-center text-white font-semibold">
+                    <p>
+                      <span className="text-cyan-500">{totalMinted}</span> /{' '}
+                      {maxSupply}
+                    </p>
+                  </div>
+
+                  <img
+                    src={foff}
+                    className="object-cover w-full sm:h-[280px] md:w-[250px] rounded-md"
+                  />
                 </div>
 
-                <img
-                  src={foff}
-                  className="object-cover w-full sm:h-[280px] md:w-[250px] rounded-md"
-                />
-              </div>
-
-              <div className="flex flex-col items-center w-full px-4 mt-16 md:mt-0">
-                {/* <div className="font-coiny flex items-center justify-between w-full">
-                  <button
-                    className="w-14 h-10 md:w-16 md:h-12 flex items-center justify-center text-brand-background hover:shadow-lg bg-gray-300 font-bold rounded-md"
-                    onClick={incrementMintAmount}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-6 w-6 md:h-8 md:w-8"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
+                <div className="flex flex-col items-center w-full px-4 mt-16 md:mt-0">
+                  {/* <div className="font-coiny flex items-center justify-between w-full">
+                    <button
+                      className="w-14 h-10 md:w-16 md:h-12 flex items-center justify-center text-brand-background hover:shadow-lg bg-gray-300 font-bold rounded-md"
+                      onClick={incrementMintAmount}
                     >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-                      />
-                    </svg>
-                  </button>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6 md:h-8 md:w-8"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                        />
+                      </svg>
+                    </button>
 
-                  <p className="flex items-center justify-center flex-1 grow text-center font-bold text-brand-pink text-3xl md:text-4xl">
-                    {mintAmount}
+                    <p className="flex items-center justify-center flex-1 grow text-center font-bold text-brand-pink text-3xl md:text-4xl">
+                      {mintAmount}
+                    </p>
+
+                    <button
+                      className="w-14 h-10 md:w-16 md:h-12 flex items-center justify-center text-brand-background hover:shadow-lg bg-gray-300 font-bold rounded-md"
+                      onClick={decrementMintAmount}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6 md:h-8 md:w-8"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M18 12H6"
+                        />
+                      </svg>
+                    </button>
+                  </div> */}
+
+                  <p className="text-md text-pink-200 tracking-widest mt-3">
+                    Max Mint Amount: {maxMintAmount}
                   </p>
 
-                  <button
-                    className="w-14 h-10 md:w-16 md:h-12 flex items-center justify-center text-brand-background hover:shadow-lg bg-gray-300 font-bold rounded-md"
-                    onClick={decrementMintAmount}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-6 w-6 md:h-8 md:w-8"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M18 12H6"
-                      />
-                    </svg>
-                  </button>
-                </div> */}
+                  <div className="border-t border-b py-4 mt-12 w-full">
+                    <div className="w-full text-xl font-coiny flex items-center justify-between text-blue-50">
+                      <p>Total</p>
 
-                <p className="text-md text-pink-200 tracking-widest mt-3">
-                  Max Mint Amount: {maxMintAmount}
-                </p>
-
-                <div className="border-t border-b py-4 mt-12 w-full">
-                  <div className="w-full text-xl font-coiny flex items-center justify-between text-blue-50">
-                    <p>Total</p>
-
-                    <div className="flex items-center space-x-3 uppercase">
-                      <p>
-                        {config.price > 0 ? `${Number.parseFloat(config.price * mintAmount).toFixed(2)} ETH` : 'free!'}
-                      </p>{' '}
-                      <span className="text-gray-400">+ gas</span>
+                      <div className="flex items-center space-x-3 uppercase">
+                        <p>
+                          {config.price > 0 ? `${Number.parseFloat(config.price * mintAmount).toFixed(2)} ETH` : 'free!'}
+                        </p>{' '}
+                        <span className="text-gray-400">+ gas</span>
+                      </div>
                     </div>
                   </div>
+
+                  {/* Mint Button && Connect Wallet Button */}
+                  {wallet ? (
+                    <button
+                      className={` ${paused || isMinting
+                        ? 'bg-gray-900 cursor-not-allowed'
+                        : 'bg-gradient-to-br from-blue-500 to-cyan-500 shadow-lg hover:shadow-blue-400/50'
+                        } font-coiny mt-12 w-full px-6 py-3 rounded-md text-2xl text-white  mx-4 tracking-wide uppercase`}
+                      disabled={paused || isMinting}
+                      onClick={isPreSale ? presaleMintHandler : publicMintHandler}
+                    >
+                      {isMinting ? 'Minting...' : 'Mint'}
+                    </button>
+                  ) : (
+                    <button
+                      className="font-coiny mt-12 w-full bg-gradient-to-br from-blue-500 to-cyan-500 shadow-lg px-6 py-3 rounded-md text-2xl text-white hover:shadow-blue-400/50 mx-4 tracking-wide uppercase"
+                      onClick={() => connect()}
+                    >
+                      Connect Wallet
+                    </button>
+                  )}
                 </div>
-
-                {/* Mint Button && Connect Wallet Button */}
-                {wallet ? (
-                  <button
-                    className={` ${paused || isMinting
-                      ? 'bg-gray-900 cursor-not-allowed'
-                      : 'bg-gradient-to-br from-blue-500 to-cyan-500 shadow-lg hover:shadow-blue-400/50'
-                      } font-coiny mt-12 w-full px-6 py-3 rounded-md text-2xl text-white  mx-4 tracking-wide uppercase`}
-                    disabled={paused || isMinting}
-                    onClick={isPreSale ? presaleMintHandler : publicMintHandler}
-                  >
-                    {isMinting ? 'Minting...' : 'Mint'}
-                  </button>
-                ) : (
-                  <button
-                    className="font-coiny mt-12 w-full bg-gradient-to-br from-blue-500 to-cyan-500 shadow-lg px-6 py-3 rounded-md text-2xl text-white hover:shadow-blue-400/50 mx-4 tracking-wide uppercase"
-                    onClick={() => connect()}
-                  >
-                    Connect Wallet
-                  </button>
-                )}
               </div>
-            </div>
 
-            {/* Status */}
-            {status && (
-              <div
-                className={`border ${status.success ? 'border-green-500' : 'border-brand-pink-400 '
-                  } rounded-md text-start h-full px-4 py-4 w-full mx-auto mt-8 md:mt-4"`}
-              >
-                <p className="flex flex-col space-y-2 text-white text-sm md:text-base break-words ...">
-                  {status.message}
-                </p>
+              {/* Status */}
+              {status && (
+                <div
+                  className={`border ${status.success ? 'border-green-500' : 'border-brand-pink-400 '
+                    } rounded-md text-start h-full px-4 py-4 w-full mx-auto mt-8 md:mt-4"`}
+                >
+                  <p className="flex flex-col space-y-2 text-white text-sm md:text-base break-words ...">
+                    {status.message}
+                  </p>
+                </div>
+              )}
+
+              {/* Contract Address */}
+              <div className="border-t border-gray-800 flex flex-col items-center mt-5 py-5 w-full">
+                <h3 className="font-coiny text-2xl text-blue-50 uppercase mt-6">
+                  Contract Address
+                </h3>
+                <a
+                  href={`https://rinkeby.etherscan.io/address/${config.contractAddress}#readContract`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-gray-400 mt-2"
+                >
+                  <span className="break-all ...">{config.contractAddress}</span>
+                </a>
               </div>
-            )}
-
-            {/* Contract Address */}
-            <div className="border-t border-gray-800 flex flex-col items-center mt-5 py-5 w-full">
-              <h3 className="font-coiny text-2xl text-blue-50 uppercase mt-6">
-                Contract Address
-              </h3>
-              <a
-                href={`https://rinkeby.etherscan.io/address/${config.contractAddress}#readContract`}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-gray-400 mt-2"
-              >
-                <span className="break-all ...">{config.contractAddress}</span>
-              </a>
             </div>
           </div>
         </div>
--- src/PageNotFound.js
+++ src/PageNotFound.js
@@ -3,14 +3,19 @@ import { Link } from "react-router-dom";
 import logo from './images/puddlegoose-outlined-logo.png'
 import './styles/globals.css';
 
+import Header from './Header';
+
 export default function PageNotFound() {
     return (
-        <div className="grid h-screen place-items-center">
-            <div className="font-coiny uppercase font-bold text-white text-3xl">
-                Sorry no frens here, <Link className="text-rose-500 hover:text-white transition ease-in-out delay-30" to="/">maybe try over here!</Link>
-            </div>
-            <div className='content-center'>
-                <img className="mt-0 pb-10 sm:mt-20" src={logo} alt="" />
+        <div className="min-h-screen h-full w-full flex flex-col overflow-hidden">
+            <Header />
+            <div className="grid h-screen place-items-center">
+                <div className="font-coiny uppercase font-bold text-white text-3xl">
+                    Sorry no frens here, <Link className="text-rose-500 hover:text-white transition ease-in-out delay-30" to="/">maybe try over here!</Link>
+                </div>
+                <div className='content-center'>
+                    <img className="mt-0 pb-10 sm:mt-20" src={logo} alt="" />
+                </div>
             </div>
         </div>
     )
